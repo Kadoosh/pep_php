cf5a2a90590a5e3ef5eca7a33596bf7ba6226b96
Rafael Pereira <eletroriver@gmail.com>
2022-12-20 21:59:33 -0300

    add

--- operators.php
+++ operators.php
@@ -2,11 +2,11 @@
 
     //Operadores aritmeticos
 
-    echo 1 + 1; //Soma
-    echo 2 - 1; //Subtração
-    echo 2 * 2; //Multiplicação
-    echo 4 / 2; //Divisão
-    echo "<br>";
+    //echo 1 + 1; //Soma
+    //echo 2 - 1; //Subtração
+    //echo 2 * 2; //Multiplicação
+    //echo 4 / 2; //Divisão
+    //echo "<br>";
     echo 3 + 2 * 5; //Obtem resultado 13... Obedecendo a regra matematica de multiplicação primeiro
     echo "<br>";
     echo (3 + 2) * 5; //Obtem resultado 25... E possivel obrigar como queremos a operação ultilizando ()
@@ -16,9 +16,19 @@
     $b = 1;
     $c = 2;
 
-    $d = $a * $b / $c + $a + ($c - $a);
+    $d = $a * $b / $c;
 
     echo $d;
 
+    //Operadores de modulo
+
+    $e = 20;
+    $f = 4;
+
+
+    echo $e % $f;
+    echo " = Modulo <br>";
+    echo $e / $f;
+    echo " = Divisao <br>";
 
 ?>
