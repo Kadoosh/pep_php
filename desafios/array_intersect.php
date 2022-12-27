0ededb3a4080a6258bced61daa6a676a39fec19e
Rafael Pereira <eletroriver@gmail.com>
2022-12-27 15:39:13 -0300

    add array_intersect

---
+++ desafios/array_intersect.php
@@ -0,0 +1,30 @@
+<?php 
+
+     $fruits1 = ["banana", "maça", "pera", "melancia", "Laranja"];
+     $fruits2 = ["banana", "maça", "pera", "melancia", "Laranja", "Uva"];
+     $result = [];
+
+     $result = array_intersect($fruits1, $fruits2);
+
+     //Forma nativa PHP
+     
+     echo "Forma nativa PHP: <br>";
+     print_r($result);
+     echo "<br>";
+
+     //Forma feita a mão
+
+     function arrCompare(array $arr1, array $arr2){
+
+        foreach ($arr1 as $key => $value) {
+            if (in_array($value, $arr2)) {
+                $equal[$key] = $value;
+            }
+        }
+        echo "Forma feita a mão: <br>";
+        print_r($equal);
+     }
+
+     arrCompare($fruits1, $fruits2);
+
+?>
