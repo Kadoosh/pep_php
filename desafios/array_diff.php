cd90f41ce6294261fb346fef2c7c733a14b145bb
Rafael Pereira <eletroriver@gmail.com>
2022-12-29 15:36:32 -0300

    update array_diff

--- desafios/array_diff.php
+++ desafios/array_diff.php
@@ -2,35 +2,23 @@
 
     $names1 = ["lucas", "pedro", "maria", "joao", "marcelo", "ana" ];
     $names2 = ["lucas", "joao", "pedro", "marcelo"];
-    $difference0 = [];
-    $difference1 = [];
 
     //Forma nativa do PHP
 
-    $difference0 = array_diff($names1, $names2);
-
     echo "Forma nativa PHP: <br>";
-    print_r($difference0);
-
-    echo "<br>";
+    print_r($difference0 = array_diff($names1, $names2));
 
     //For feita a mão
 
     function arrDiff(array $arr1, array $arr2){
-        
         foreach ($arr1 as $key => $value) {
-            if (in_array($value, $arr2)) {
-                continue;
-            } else {
+            if (!in_array($value, $arr2)) {
                 $diff[$key] = $value;
             }  
         }
-
         print_r($diff);
     }
-
-    echo "Forma feita a mão: <br>";
+    echo "<br>Forma feita a mão: <br>";
     arrDiff($names1, $names2);
-
 ?>
     
